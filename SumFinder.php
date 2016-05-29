9aa6459d169fb3bae92cc633384cfa0e29a90a59
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-05-29 15:56:53 -0300

    finish test

--- SumFinder.php
+++ SumFinder.php
@@ -5,7 +5,32 @@
  * @param array $inputArray
  * @return array
  */
-function sumFinder(array $inputArray){}
+function sumFinder(array $inputArray){
+    $arrayGroups = array();
+
+    foreach($inputArray as $element){
+        //initial settings
+        if(!isset($previousElement)){
+            $previousElement = $element;
+            $arrayGroupNumber = 0;
+        }
+
+        if(($previousElement + 1 ) != $element){
+            $arrayGroupNumber += 1;
+        }
+
+        $arrayGroups[$arrayGroupNumber][] = $element;
+        $previousElement = $element;
+    }
+
+    usort($arrayGroups, 'compareArrays');
+    $highestGroup = array_pop($arrayGroups);
+
+    return array(
+        "group" => implode(", ", $highestGroup),
+        "sum" => array_sum($highestGroup)
+    );
+}
 
 /**
  * Custom array comparison method
